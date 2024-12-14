2447bf5add727ce2b0b2be2415fcd3e9e1c8b213
Harry Hogg <harry@hogg.io>
2024-12-14 18:33:49 +0000

    Use rounded_redactions for snapshot testing

--- workspaces/tilings/src-rust/tiling/src/geometry/convex_hull_tests.rs
+++ workspaces/tilings/src-rust/tiling/src/geometry/convex_hull_tests.rs
@@ -31,36 +31,51 @@ fn creates_an_empty_convex_hull_from_2_line_segments() {
 #[test]
 fn orders_points_correctly_from_triangle_line_segments() {
   let polygon = Polygon::default().with_shape(Shape::Triangle).at_center();
+  let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
 
-  assert_json_snapshot!(ConvexHull::from_line_segments(polygon.line_segments.iter()));
+  assert_json_snapshot!(convex_hull, {
+    ".points[].*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn orders_points_correctly_from_square_line_segments() {
   let polygon = Polygon::default().with_shape(Shape::Square).at_center();
+  let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
 
-  assert_json_snapshot!(ConvexHull::from_line_segments(polygon.line_segments.iter()));
+  assert_json_snapshot!(convex_hull, {
+    ".points[].*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn orders_points_correctly_from_hexagon_line_segments() {
   let polygon = Polygon::default().with_shape(Shape::Hexagon).at_center();
+  let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
 
-  assert_json_snapshot!(ConvexHull::from_line_segments(polygon.line_segments.iter()));
+  assert_json_snapshot!(convex_hull, {
+    ".points[].*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn orders_points_correctly_from_octagon_line_segments() {
   let polygon = Polygon::default().with_shape(Shape::Hexagon).at_center();
+  let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
 
-  assert_json_snapshot!(ConvexHull::from_line_segments(polygon.line_segments.iter()));
+  assert_json_snapshot!(convex_hull, {
+    ".points[].*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn orders_points_correctly_from_dodecagon_line_segments() {
   let polygon = Polygon::default().with_shape(Shape::Dodecagon).at_center();
+  let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
 
-  assert_json_snapshot!(ConvexHull::from_line_segments(polygon.line_segments.iter()));
+  assert_json_snapshot!(convex_hull, {
+    ".points[].*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
@@ -69,50 +84,57 @@ fn finds_line_segments_for_vector_terminal_points_top() {
   // it's sides are flat along the top and bottom.
   let polygon = Polygon::default().with_shape(Shape::Square).at_center();
   let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
+  let line_segment = convex_hull.find_line_segment(Point::default().with_x(0.0).with_y(1.0));
 
-  assert_json_snapshot!(convex_hull.find_line_segment(
-    Point::default()
-      .with_x(0.0)
-      .with_y(-1.0)
-      .rotate(f64::consts::PI * 0.0, None)
-  ));
+  assert_json_snapshot!(line_segment, {
+    ".*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn finds_line_segments_for_vector_terminal_points_right() {
   let polygon = Polygon::default().with_shape(Shape::Square).at_center();
   let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
-
-  assert_json_snapshot!(convex_hull.find_line_segment(
+  let line_segment = convex_hull.find_line_segment(
     Point::default()
       .with_x(0.0)
       .with_y(-1.0)
-      .rotate(f64::consts::PI * 0.5, None)
-  ));
+      .rotate(f64::consts::PI * 0.5, None),
+  );
+
+  assert_json_snapshot!(line_segment, {
+    ".*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn finds_line_segments_for_vector_terminal_points_bottom() {
   let polygon = Polygon::default().with_shape(Shape::Square).at_center();
   let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
-
-  assert_json_snapshot!(convex_hull.find_line_segment(
+  let line_segment = convex_hull.find_line_segment(
     Point::default()
       .with_x(0.0)
       .with_y(-1.0)
-      .rotate(f64::consts::PI, None)
-  ));
+      .rotate(f64::consts::PI, None),
+  );
+
+  assert_json_snapshot!(line_segment, {
+    ".*" => insta::rounded_redaction(5)
+  });
 }
 
 #[test]
 fn finds_line_segments_for_vector_terminal_points_left() {
   let polygon = Polygon::default().with_shape(Shape::Square).at_center();
   let convex_hull = ConvexHull::from_line_segments(polygon.line_segments.iter());
-
-  assert_json_snapshot!(convex_hull.find_line_segment(
+  let line_segment = convex_hull.find_line_segment(
     Point::default()
       .with_x(0.0)
       .with_y(-1.0)
-      .rotate(f64::consts::PI * 1.5, None)
-  ));
+      .rotate(f64::consts::PI * 1.5, None),
+  );
+
+  assert_json_snapshot!(line_segment, {
+    ".*" => insta::rounded_redaction(5)
+  });
 }
